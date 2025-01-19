9d05ba2cc33dda0f5eb3dca16644da4d45c7d4bc
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2025-01-19 12:39:34 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Masala 1: Sonni toq yoki juftligini aniqlash
+        //Console.Write("Enter any number: ");
+        //int number = int.Parse(Console.ReadLine());
+
+        //string result = (number == 0) ? "Bu son 0 teng" : (number % 2 == 0) ? " Bu son juft son": "Toq son";
+
+        //Console.WriteLine(result);
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+        //Masala 2: Kvadrat tenglama yechimi
+        //Ax^2+Bx + C = 0
+
+        //Console.Write("A number enter:");
+        //double a = double.Parse(Console.ReadLine());
+        //Console.Write("B number enter:");
+        //double b = double.Parse(Console.ReadLine());
+        //Console.Write("C number enter:");
+        //double c = double.Parse(Console.ReadLine());
+
+        //double x_1, x_2, d;
+
+        //d = Math.Pow(b, 2) - 4 * a * c;
+
+        //if (a == 0)
+        //{
+        //    x_1 = -c / b;
+        //    Console.WriteLine($"Natija: x_1={x_1}");
+        //}
+        //else if (d < 0)
+        //{
+        //    Console.WriteLine("D 0 ga teng bulishi mumkin emas");
+        //}
+        //else
+        //{
+        //    x_1 = (-b + Math.Sqrt(d)) / 2 * a;
+        //    x_2 = (-b - Math.Sqrt(d)) / 2* a;
+
+        //    Console.WriteLine($"x_1 = {x_1}");
+        //    Console.WriteLine($"x_2 = {x_2}");
+        // }
+
+
+        Console.Write("First number:");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Second number:");
+        int b = int.Parse(Console.ReadLine());
+
+        int result;
+        char operation = 
+
+        
+
+    }
+}
